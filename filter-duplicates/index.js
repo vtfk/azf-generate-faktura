e9205bd9d71d117954ba3f832dbd9fbb0cdcc8e0
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-04-12 10:01:22 +0200

    Added get-recipients

---
+++ filter-duplicates/index.js
@@ -0,0 +1,23 @@
+const { logger } = require('../lib/logger')('filter-duplicates')
+const filterDuplcates = require('../lib/filter-duplicates')
+
+
+module.exports = async function (context, req) {
+    logger('INFO', 'Received request')
+    try {
+        if (!req.body) throw Error('Include body in request')
+        const persons = Array.isArray(req.body) ? req.body : [req.body]
+        const uniquePersons = filterDuplcates(persons)
+
+        context.res = {
+            status: 200,
+            body: uniquePersons
+        }
+        
+    } catch (error) {
+        context.res = {
+            status: 400,
+            body: "Please pass an array of objects with at least a key 'Personnr'"
+        }
+    }
+};
---
+++ get-invoice/index.js
@@ -0,0 +1,22 @@
+const { logger } = require('../lib/logger')('get-invoice')
+const getInvoice = require('../lib/get-invoice')
+
+module.exports = async function (context, req) {
+    logger('INFO', 'Received request')
+    try {
+        if (!req.body && req.body['PC-kode']) throw Error('Include body in request')
+
+        const invoice = getInvoice(req.body['PC-kode'])
+        // TODO: Return something else if that pc-code does not exist?
+        context.res = {
+            status: 200,
+            body: invoice
+        }
+        
+    } catch (error) {
+        context.res = {
+            status: 400,
+            body: 'Body did not include the key "PC-kode"' + error
+        }
+    }
+};
