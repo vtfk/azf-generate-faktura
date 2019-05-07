cfdcbf9c541cb783dfc82c4dd6f3891447795299
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-05-07 16:13:26 +0200

    Added get-recipients

--- get-recipients/index.js
+++ get-recipients/index.js
@@ -1,16 +1,28 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const getGraphQL = require("../lib/get-graphql")
+const formatRecipients = require('../lib/format-recipients')
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+module.exports = async function(context, req) {
+  if (req.body && Array.isArray(req.body)) {
+    try {
+      const personNrs = req.body.map(person => person.personNr)
+      let { persons } = await getGraphQL(personNrs)
+      persons = persons.map(person => formatRecipients(person))
+      context.res = {
+        status: 200,
+        body: persons
+      }
+    } catch (error) {
+      context.log.error(error)
+
+      context.res = {
+        status: 500,
+        body: "Something went wrong while processing the request."
+      }
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+  } else {
+    context.res = {
+      status: 400,
+      body: "Please pass an array with at least 'personNr' and 'school' keys in the body."
     }
-};
+  }
+}
