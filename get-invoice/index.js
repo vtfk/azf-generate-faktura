c06d5a855b00acbf17188df630375d6df6880e7a
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-05-07 16:18:15 +0200

    Var-name change

--- get-invoice/index.js
+++ get-invoice/index.js
@@ -4,9 +4,9 @@ const getInvoice = require('../lib/get-invoice')
 module.exports = async function (context, req) {
     logger('INFO', 'Received request')
     try {
-        if (!req.body && req.body['PcCode']) throw Error('Include body in request')
+        if (!req.body && req.body['pcCode']) throw Error('Include body in request')
 
-        const invoice = getInvoice(req.body['PcCode'])
+        const invoice = getInvoice(req.body['pcCode'])
         // TODO: Return something else if that pc-code does not exist?
         context.res = {
             status: 200,
@@ -16,7 +16,7 @@ module.exports = async function (context, req) {
     } catch (error) {
         context.res = {
             status: 400,
-            body: 'Body did not include the key "PcCode"' + error
+            body: 'Body did not include the key "pcCode"' + error
         }
     }
 };
