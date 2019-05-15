08b8161e0efef560ef80b637fb4ffbd9cd7c49ff
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-05-15 11:54:13 +0200

    Better error handling in get-invoice

--- get-invoice/index.js
+++ get-invoice/index.js
@@ -3,20 +3,25 @@ const getInvoice = require('../lib/get-invoice')
 
 module.exports = async function (context, req) {
     logger('INFO', 'Received request')
-    try {
-        if (!req.body && req.body['pcCode']) throw Error('Include body in request')
+    if (!req.body || !req.body['pcCode']) {
+        context.res = {
+            status: 400,
+            body: 'Body did not include the key "pcCode"'
+        }
+        return
+    }
 
+    try {
         const invoice = getInvoice(req.body['pcCode'])
-        // TODO: Return something else if that pc-code does not exist?
         context.res = {
             status: 200,
             body: invoice
         }
-        
     } catch (error) {
         context.res = {
             status: 400,
-            body: 'Body did not include the key "pcCode"' + error
+            body: 'The requested PC-Code was not found'
         }
     }
+    
 };
